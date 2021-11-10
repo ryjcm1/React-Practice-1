198ab13696d175ad3ea14a904865f6d3acdac50d
Jimmy Chuk <jimmychuk@outlook.com>
2021-11-09 21:06:34 -0500

    stateful list of expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,39 +11,26 @@ const Expenses = (props) => {
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    
   };
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
-        //two way binding = controlled component
+          //two way binding = controlled component
           onChangefilter={filterChangeHandler}
           selected={filteredYear}
         />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          date={props.expenses[0].date}
-          amount={props.expenses[0].amount}
-        ></ExpenseItem>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))}
 
-        <ExpenseItem
-          title={props.expenses[1].title}
-          date={props.expenses[1].date}
-          amount={props.expenses[1].amount}
-        ></ExpenseItem>
-
-        <ExpenseItem
-          title={props.expenses[2].title}
-          date={props.expenses[2].date}
-          amount={props.expenses[2].amount}
-        ></ExpenseItem>
-
-        <ExpenseItem
-          title={props.expenses[3].title}
-          date={props.expenses[3].date}
-          amount={props.expenses[3].amount}
-        ></ExpenseItem>
+        
       </Card>
     </div>
   );
